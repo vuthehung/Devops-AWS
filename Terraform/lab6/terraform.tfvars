c5d59ef434b4c3089291a8edcdc29434b706f0c0
vuthehung <hungvt280102@gmail.com>
2024-04-15 15:17:31 +0700

    terraform lab6 part 2 create module compute, security

--- Terraform/lab6/terraform.tfvars
+++ Terraform/lab6/terraform.tfvars
@@ -4,3 +4,5 @@ availability_zone_2 = "ap-southeast-1b"
 cidr_block          = "10.0.0.0/16"
 public_subnet_ips   = ["10.0.1.0/24", "10.0.2.0/24"]
 private_subnet_ips  = ["10.0.10.0/24", "10.0.20.0/24"]
+keypair_path        = "./key-pair/hungdev-key.pub"
+instance_type       = "t2.micro"
--- Terraform/lab6/variable.tf
+++ Terraform/lab6/variable.tf
@@ -24,3 +24,21 @@ variable "private_subnet_ips" {
   type     = list(string)
   nullable = false
 }
+
+#parameter for compute module
+variable "instance_type" {
+  type        = string
+  description = "Type of EC2 instance to launch. Example: t2.micro"
+  default     = "t2.micro"
+}
+variable "amis" {
+  type = map(any)
+  default = {
+    "ap-southeast-1" : "ami-0910e4162f162c238"
+    "ap-northeast-1" : "ami-0bdd30a3e20da30a1"
+  }
+}
+variable "keypair_path" {
+  type    = string
+  default = "./key-pair/hungdev-key.pub"
+}
